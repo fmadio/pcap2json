bb2ac1417d5ff9e06cdd02e3551ea1be16bf306d
Linux User <aaron@fmad.io>
2019-01-07 18:18:59 +0900

    move FMAD packet format into fTypes

--- flow.h
+++ flow.h
@@ -4,35 +4,6 @@
 struct Output_t;
 struct FlowIndex_t;
 
-typedef struct FMADPacket_t
-{
-	u64             TS;                     // 64bit nanosecond epoch
-
-	u32             LengthCapture   : 16;   // length captured
-	u32             LengthWire      : 16;   // Length on the wire
-
-	u32             PortNo          :  8;   // Port number
-	u32             Flag            :  8;   // flags
-	u32             pad0            : 16;
-
-} __attribute__((packed)) FMADPacket_t;
-
-// header per packet
-typedef struct FMADHeader_t
-{
-	u16				PktCnt;					// number of packets
-	u16				pad0;
-
-	u32				BytesWire;				// total wire bytes  
-	u32				BytesCapture;			// total capture bytes 
-	u32				Length;					// length of this block in bytes
-
-	u64				TSStart;				// TS of first packet
-	u64				TSEnd;					// TS of last packet 
-
-} __attribute__((packed)) FMADHeader_t;
-
-
 typedef struct PacketBuffer_t
 {
 	u32						PktCnt;				// number of packets in this buffer
