6fe1ab099e914e6c7bb81e44656c99ea631fd917
Linux User <aaron@fmad.io>
2019-08-13 13:53:01 +0900

    update performance stats using 72B slicing from streamcat

--- flow.c
+++ flow.c
@@ -11,7 +11,6 @@
 //
 // on the interop timecompressed(x100) + (192B sliced) data following performance is seen
 //
-// sudo stream_cat --chunked --cpu 63 interop_20190812_1755 --pktslice 128 | /mnt/store0/git/pcap2json/pcap2json  --config /mnt/store0/etc/pcap2json.config
 //
 // interop_scaled_20181204_2054     15GB Chunk(Cnt:   63842 Start: 4100250 End: 4164091 Comp:1.49) Inv:-nan Cap:-nan CacheI:-nan Cache:-nan Disk:-nan Drop:-nan Pkt:0
 //
@@ -100,6 +99,20 @@
 //
 // PCAPWall time: 16949728256.00 sec ProcessTime 12.78 sec (0.000)
 // Total Time: 12.78 sec RawInput[Wire 61.713 Gbps Capture 9.604 Gbps 6.789 Mpps] Output[0.000 Gbps] TotalLine:0 0 Line/Sec
+//
+// 2019/8/13:
+// using stream_cat slice 72B (instead of 128B) not sure if the above was using 128B or 72B
+//
+// sudo stream_cat --chunked --cpu 63 interop_20190812_1755 --pktslice 128 | /mnt/store0/git/pcap2json/pcap2json  --config /mnt/store0/etc/pcap2json.config
+//
+// [13:39:55.219.739.136] 44.601/0.000 GB 10.37 Mpps 94.07 Gbps | cat   8214 MB 1.00 0.57 0.31 | Flows/Snap:  41624 FlowCPU:0.38 | ESPush:     0   0.00K ESErr    0 | OutCPU:0.00 OutPush: 0.00 MB OutQueue: 581.8MB
+// [13:39:55.593.488.896] 55.603/0.000 GB 10.36 Mpps 94.50 Gbps | cat   6306 MB 1.00 0.57 0.31 | Flows/Snap:  42331 FlowCPU:0.38 | ESPush:     0   0.00K ESErr    0 | OutCPU:0.00 OutPush: 0.00 MB OutQueue: 687.7MB
+// [13:39:55.955.025.664] 66.539/0.000 GB 10.41 Mpps 93.93 Gbps | cat   4394 MB 1.00 0.57 0.31 | Flows/Snap:  42148 FlowCPU:0.38 | ESPush:     0   0.00K ESErr    0 | OutCPU:0.00 OutPush: 0.00 MB OutQueue: 831.5MB
+// [13:39:56.330.177.792] 77.589/0.000 GB 10.47 Mpps 94.91 Gbps | cat   2469 MB 1.00 0.57 0.31 | Flows/Snap:  45309 FlowCPU:0.38 | ESPush:     0   0.00K ESErr    0 | OutCPU:0.00 OutPush: 0.00 MB OutQueue: 976.6MB
+// [13:39:56.704.730.112] 88.713/0.000 GB 10.47 Mpps 95.55 Gbps | cat    541 MB 1.00 0.57 0.31 | Flows/Snap:  47187 FlowCPU:0.38 | ESPush:     0   0.00K ESErr    0 | OutCPU:0.00 OutPush: 0.00 MB OutQueue:1095.5MB
+//
+// PCAPWall time: 16949728256.00 sec ProcessTime 10.96 sec (0.000)
+// Total Time: 10.96 sec RawInput[Wire 71.950 Gbps Capture 11.197 Gbps 7.915 Mpps] Output[0.000 Gbps] TotalLine:0 0 Line/Sec
 //
 //---------------------------------------------------------------------------------------------
 
