566d95cc8f952822e1f9e96520ee25b5826d0275
Linux User <aaron@fmad.io>
2020-03-18 18:07:18 +0900

    remove old files

--- es_count.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/local/bin/bash
-/usr/local/bin/curl -H "Content-Type: application/json"  -XGET "192.168.2.176:9200/pcap2json_test7/_count" | jq 
-
--- es_last.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/local/bin/bash
-curl -H "Content-Type: application/json"    -XPOST "http://192.168.2.115:9200/pcap2json_test7/_search" --data-binary "@last.json" | jq
-
