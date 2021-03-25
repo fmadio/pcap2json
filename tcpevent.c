cb599c9e3d933ef0b37ecb1854995249dd379410
Amaan Cheval <amaan.cheval@gmail.com>
2021-03-25 14:49:28 +0530

    minor/style: Tabify tcpevent.c

--- tcpevent.c
+++ tcpevent.c
@@ -14,30 +14,30 @@ u32 TCPEventDump(u8* OutputStr, u64 TS, TCPHeader_t* tcp_header, FlowRecord_t* F
 
 void FlowPktToTCPFullDup(FlowRecord_t* FlowPkt, TCPFullDup_t* TCPFullDup)
 {
-    TCPFullDup->IPProto = FlowPkt->IPProto;
+	TCPFullDup->IPProto = FlowPkt->IPProto;
 
-    // Sort FlowPkt's MAC src-dst deterministically and set to TCPFullDup's A/B
-    // accordingly
-    s8 cmp = memcmp(FlowPkt->IPSrc, FlowPkt->IPDst, sizeof(FlowPkt->IPSrc));
-    if (cmp == 0)
-    {
-        cmp = FlowPkt->PortSrc < FlowPkt->PortDst ? 1 : -1;
-    }
+	// Sort FlowPkt's MAC src-dst deterministically and set to TCPFullDup's A/B
+	// accordingly
+	s8 cmp = memcmp(FlowPkt->IPSrc, FlowPkt->IPDst, sizeof(FlowPkt->IPSrc));
+	if (cmp == 0)
+	{
+		cmp = FlowPkt->PortSrc < FlowPkt->PortDst ? 1 : -1;
+	}
 
-    if (cmp > 0)
-    {
-        memcpy(TCPFullDup->IP_A, FlowPkt->IPSrc, sizeof(FlowPkt->IPSrc));
-        memcpy(TCPFullDup->IP_B, FlowPkt->IPDst, sizeof(FlowPkt->IPDst));
+	if (cmp > 0)
+	{
+		memcpy(TCPFullDup->IP_A, FlowPkt->IPSrc, sizeof(FlowPkt->IPSrc));
+		memcpy(TCPFullDup->IP_B, FlowPkt->IPDst, sizeof(FlowPkt->IPDst));
 
-        TCPFullDup->PortA = FlowPkt->PortSrc;
-        TCPFullDup->PortB = FlowPkt->PortDst;
-    }
-    else
-    {
-        memcpy(TCPFullDup->IP_A, FlowPkt->IPDst, sizeof(FlowPkt->IPDst));
-        memcpy(TCPFullDup->IP_B, FlowPkt->IPSrc, sizeof(FlowPkt->IPSrc));
+		TCPFullDup->PortA = FlowPkt->PortSrc;
+		TCPFullDup->PortB = FlowPkt->PortDst;
+	}
+	else
+	{
+		memcpy(TCPFullDup->IP_A, FlowPkt->IPDst, sizeof(FlowPkt->IPDst));
+		memcpy(TCPFullDup->IP_B, FlowPkt->IPSrc, sizeof(FlowPkt->IPSrc));
 
-        TCPFullDup->PortA = FlowPkt->PortDst;
-        TCPFullDup->PortB = FlowPkt->PortSrc;
-    }
+		TCPFullDup->PortA = FlowPkt->PortDst;
+		TCPFullDup->PortB = FlowPkt->PortSrc;
+	}
 }
