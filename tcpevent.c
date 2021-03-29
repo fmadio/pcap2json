9648da3d9bf0dac73c22af84db018f433d7cb36a
Amaan Cheval <amaan.cheval@gmail.com>
2021-03-29 10:38:47 +0530

    tcpevent: Add FLAG to TCPEvent's MSB (and OP for 3 LSBs)
    Subject to change, but for now this flag is sufficient to display the direction
    of the OP for a full-duplex connection, without revealing connection details.

--- tcpevent.c
+++ tcpevent.c
@@ -3,6 +3,8 @@
 #include "fTypes.h"
 #include "flow.h"
 
+// For a 32-bit TCP event, the MSB (i.e. highest 8 bits) are for the flag mask,
+// and the bottom 24 bits are for the TCP OP itself
 enum TCP_OPS {
 	TCP_OP_NULL,
 	TCP_OP_SYN,
@@ -23,6 +25,21 @@ char *TCP_OP_STR[TCP_OP_TOTAL_COUNT] = {
 	"TCP_OP_RST",
 	"TCP_OP_FIN"
 };
+
+#define TCP_OP_FLAG_MASK 0xff000000
+#define TCP_OP_MASK ~TCP_OP_FLAG_MASK
+#define TCP_OP_FLAG_BIT_SHIFT 24
+enum TCP_OP_FLAG {
+	TCP_OP_FLAG_H0 = 1,
+	TCP_OP_FLAG_H1
+};
+char *TCP_OP_FLAG_STR[3] = {
+	"NULL",
+
+	"H0",
+	"H1"
+};
+
 
 s8 FlowPktToTCPFullDup(FlowRecord_t* FlowPkt, TCPFullDup_t* TCPFullDup)
 {
@@ -65,37 +82,56 @@ u32 TCPEventDump(u8* OutputStr, u64 TS, IP4Header_t* IP4, FlowRecord_t* FlowPkt)
 	u32 IPOffset = (IP4->Version & 0x0f)*4;
 	TCPHeader_t* TCP = (TCPHeader_t*)((u8*)IP4 + IPOffset);
 	u16 TCPFlags = swap16(TCP->Flags);
-	u8 TCPOp = TCP_OP_NULL;
+	// A TCP event log includes an MSB for TCP_FLAG and the rest is TCP_OP
+	u32 TCPEvent = TCP_OP_NULL;
 	u8* Output = OutputStr;
 
 	memset(Output, 0, strlen(Output));
 
+	// TODO: Only one TCP event / packet? Or multiple based on flags/events?
 	if (FlowPkt->IPProto == IPv4_PROTO_TCP)
 	{
+		// Set TCP OP
 		if (TCP_FLAG_SYN(TCPFlags) == 1)
 		{
 			if (TCP_FLAG_ACK(TCPFlags) == 1)
-				TCPOp = TCP_OP_SYNACK;
+				TCPEvent = TCP_OP_SYNACK;
 			else
-				TCPOp = TCP_OP_SYN;
+				TCPEvent = TCP_OP_SYN;
 		}
 		else if (FlowPkt->TCPLength == 0 && TCP_FLAG_ACK(TCPFlags) == 1)
 		{
-			TCPOp = TCP_OP_ACK;
+			TCPEvent = TCP_OP_ACK;
 		}
 		else if (FlowPkt->TCPLength != 0 && TCP_FLAG_ACK(TCPFlags) == 1)
 		{
-			TCPOp = TCP_OP_PSH;
+			TCPEvent = TCP_OP_PSH;
 		}
 		else if (TCP_FLAG_FIN(TCPFlags) == 1)
 		{
-			TCPOp = TCP_OP_FIN;
+			TCPEvent = TCP_OP_FIN;
 		}
 		else if (TCP_FLAG_RST(TCPFlags) == 1)
 		{
-			TCPOp = TCP_OP_RST;
+			TCPEvent = TCP_OP_RST;
+		}
+
+		// Set MSB of TCP OP to directional flag
+		TCPFullDup_t TCPFull = { 0 };
+		s8 cmp = FlowPktToTCPFullDup(FlowPkt, &TCPFull);
+
+		if (cmp > 0)
+		{
+			TCPEvent |= (TCP_OP_FLAG_H0 << TCP_OP_FLAG_BIT_SHIFT);
+		}
+		else
+		{
+			TCPEvent |= (TCP_OP_FLAG_H1 << TCP_OP_FLAG_BIT_SHIFT);
 		}
 	}
+
+	u8 TCPOp = TCPEvent & TCP_OP_MASK;
+	u8 TCPFlag = (TCPEvent & TCP_OP_FLAG_MASK) >> TCP_OP_FLAG_BIT_SHIFT;
 
 	// TODO: Analyze TCP Options and log state change related info (window size, TS)
 
@@ -109,10 +145,11 @@ u32 TCPEventDump(u8* OutputStr, u64 TS, IP4Header_t* IP4, FlowRecord_t* FlowPkt)
 	FormatTSStr(TStr, TS);
 
 	Output += sprintf(Output,
-					  "{\"timestamp\":%f,\"TS\":\"%s\",\"tcp_op\":\"%s\",\"seq\":%u,\"ack\":%u,\"len\":%u",
+					  "{\"timestamp\":%f,\"TS\":\"%s\",\"tcp_op\":\"%s_%s\",\"seq\":%u,\"ack\":%u,\"len\":%u",
 					  TS/1e6,
 					  TStr,
 					  TCP_OP_STR[TCPOp],
+					  TCP_OP_FLAG_STR[TCPFlag],
 					  TCP->SeqNo,
 					  TCP->AckNo,
 					  FlowPkt->TCPLength);
